1e58b54640425a3a3d7c1a8c28f893f1cc22fd8c
orion1981 <cstopek@hotmail.com>
2017-08-29 19:10:27 -0400

    installed react router dom and started on app page

--- pp-client/src/App.js
+++ pp-client/src/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import { Button, Container } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <div className="App">
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Welcome to React</h2>
+            </div>
+            <p className="App-intro">
+              <div>
+              <ul>
+                <br/>
+                <br/>
+                <br/>
+                  <Link to="/Setup" activeStyle={{ color: 'red' }}><Container><Button className="Homepage-button" size="massive" color="blue">Kitchen Inventory</Button></Container></Link>
+                <br/>
+                <br/>
+                <br/>
+                  <Link to="/NewTeam" activeStyle={{ color: 'red' }}><Container><Button className="Homepage-button" size="massive" color="blue">High Line Inventory</Button></Container></Link>
+              </ul>
+            </div>
+            </p>
+          </div>
+        </Switch>
+      </Router>
     );
   }
 }
